c97fab2724ab0296b5a808d7b8bb6b9d9eddf366
Evan Zamir <zamir.evan@gmail.com>
2012-11-30 23:44:36 -0800

    step-4
    added mongodb support

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-/**
- * Created with JetBrains WebStorm.
- * User: evanzamir
- * Date: 11/30/12
- * Time: 11:25 PM
- * To change this template use File | Settings | File Templates.
- */
-var express = require('express');
+var express = require('express'),
+    wine = require('./routes/wines');
 
 var app = express();
 
-app.get('/wines', function(req, res) {
-    res.send([{name:'wine1'}, {name:'wine2'}]);
+app.configure(function () {
+    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
 });
-app.get('/wines/:id', function(req, res) {
-    res.send({id:req.params.id, name: "The Name", description: "description"});
-});
+
+app.get('/wines', wine.findAll);
+app.get('/wines/:id', wine.findById);
+app.post('/wines', wine.addWine);
+app.put('/wines/:id', wine.updateWine);
+app.delete('/wines/:id', wine.deleteWine);
 
 app.listen(3000);
 console.log('Listening on port 3000...');
