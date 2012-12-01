9677fbd2e62cb7ff117184ef5f916e8510d6e5c7
Evan Zamir <zamir.evan@gmail.com>
2012-11-30 23:42:02 -0800

    step-2
    added routing via wines.js

---
+++ routes/wines.js
@@ -0,0 +1,10 @@
+var express = require('express'),
+    wines = require('./routes/wines');
+
+var app = express();
+
+app.get('/wines', wines.findAll);
+app.get('/wines/:id', wines.findById);
+
+app.listen(3000);
+console.log('Listening on port 3000...');
