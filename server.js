31a67db13793781ff31ef2e14704d203acecc7dc
Evan Zamir <zamir.evan@gmail.com>
2012-11-30 23:35:29 -0800

    step-1
    Now that Express is installed, we can stub a basic REST API for the
    nodecellar application.

--- server.js
+++ server.js
@@ -5,9 +5,16 @@
  * Time: 11:25 PM
  * To change this template use File | Settings | File Templates.
  */
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(3000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:3000/');
+var express = require('express');
+
+var app = express();
+
+app.get('/wines', function(req, res) {
+    res.send([{name:'wine1'}, {name:'wine2'}]);
+});
+app.get('/wines/:id', function(req, res) {
+    res.send({id:req.params.id, name: "The Name", description: "description"});
+});
+
+app.listen(3000);
+console.log('Listening on port 3000...');
